43edc7c03974d2cc22aa85f6ba2d56160971d403
daisy212 <78274919+daisy212@users.noreply.github.com>
2025-07-31 00:08:03 +0200

    Add files via upload
    db48x version 0.9.11

--- db_interface/version.h
+++ db_interface/version.h
@@ -2,15 +2,22 @@
 #define VERSION_H
 
 
-#define DB48X_VERSION "0.9.10"
+#define DB48X_VERSION "0.9.11"
 #define PROGRAM_NAME    "DB48X"
 #define PROGRAM_VERSION DB48X_VERSION
 
+// for tests.cc compilation
+#define DBH_TSTS (1)
 
-//#define DBu585		(1)
-#define DBh743		(1)
+//#define DBu585     (1)
+#define DBh743    (1)
+#define USE_RNDIS (0)
 
-#define Db_TEST (1)
+
+#define Db_TEST (0)
+//#define SIMULATOR (1)
+
+
 
 #define HELPINDEX_NAME "/help/db48x.idx"
 #define HELPFILE_NAME "/help/db48x.md"
@@ -18,22 +25,23 @@
 
 #if  DBh743
 #define HARD_NAME "DBh743"
-#define HARD_VERSION "1.f"
+#define HARD_VERSION "2.b"
 
 #elif DBu585
 #define HARD_NAME "DBu585"
+#define HARD_VERSION "1.a"
 #else
 #define HARD_NAME "DBxxxx"
 #endif
-#define USE_EmFile	(DBh743 | DBu585)
+#define USE_EmFile   (DBh743 | DBu585)
 
 
 
 
-#define BUILD_ID	1
+#define BUILD_ID  1
 
 
-#define HBUILD_ID	1
+#define HBUILD_ID 1
 
 
 
